a618653e543623ac72624b48f5a0b98fe909d3b8
Nikola <nikola_vuchev@abv.bg>
2021-03-10 13:57:23 +0200

    04. Say Hello Extended

---
+++ src/InterfacesandAbstraction/SayHelloExtended/BasePerson.java
@@ -0,0 +1,22 @@
+package InterfacesandAbstraction.SayHelloExtended;
+
+public abstract class BasePerson implements Person {
+    private String name;
+
+    public BasePerson(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public String sayHello(){
+        return "Hello";
+    }
+}
